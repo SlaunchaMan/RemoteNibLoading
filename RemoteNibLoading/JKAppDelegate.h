ac7b47e64fc0bdfbd9cae63d070bab6c8db7e851
Jeff Kelley <SlaunchaMan@gmail.com>
2013-01-02 19:53:38 -0500

    Initial Commit

---
+++ RemoteNibLoading/JKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKAppDelegate.h
+//  RemoteNibLoading
+//
+//  Created by Jeff Kelley on 1/2/13.
+//  Copyright (c) 2013 Jeff Kelley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RemoteNibLoading/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RemoteNibLoading
+//
+//  Created by Jeff Kelley on 1/2/13.
+//  Copyright (c) 2013 Jeff Kelley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JKAppDelegate class]));
+    }
+}
